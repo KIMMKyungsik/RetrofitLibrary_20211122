17667557f28ebd9ef044f1b79bf5b5caab2af751
KIMMKyungsik <93589990+KIMMKyungsik@users.noreply.github.com>
2021-11-22 17:25:04 +0900

    Initial commit

---
+++ app/src/main/java/org/techtown/retrofitlibrary_20211122/MainActivity.kt
@@ -0,0 +1,16 @@
+package org.techtown.retrofitlibrary_20211122
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import org.techtown.retrofitlibrary_20211122.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var binding : ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
+    }
+}
