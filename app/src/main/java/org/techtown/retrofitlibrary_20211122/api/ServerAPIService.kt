c0bd60c3585f62160adb73ec7efa142fd1b71819
KIMMKyungsik <93589990+KIMMKyungsik@users.noreply.github.com>
2021-11-23 07:16:12 +0900

    우리서버가 일반적으로 내려주는 응답 형태 파싱용 클래스 추가 (BasicResponse)  - 로그인 API의 결과로 BasicResponse가 돌아온다고 명시

--- app/src/main/java/org/techtown/retrofitlibrary_20211122/api/ServerAPIService.kt
+++ app/src/main/java/org/techtown/retrofitlibrary_20211122/api/ServerAPIService.kt
@@ -1,5 +1,7 @@
 package org.techtown.retrofitlibrary_20211122.api
 
+import org.techtown.retrofitlibrary_20211122.datas.BasicResponse
+import retrofit2.Call
 import retrofit2.http.Field
 import retrofit2.http.FormUrlEncoded
 import retrofit2.http.POST
@@ -12,7 +14,8 @@ interface ServerAPIService {
     fun postRequestLogin(
         @Field("email") email: String,
         @Field("password") pw: String,
-    )
+
+    ) : Call<BasicResponse>
 
 
 }
