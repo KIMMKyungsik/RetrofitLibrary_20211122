df4b1fc05ec36cd7fea0e0f837919fa94b216f26
KIMMKyungsik <93589990+KIMMKyungsik@users.noreply.github.com>
2021-11-23 07:10:36 +0900

    실제 API 호출 세팅   - 서버 접속 기능은 모든 화면에서 필요로 함.   - BaseActivity에 멤버 변수로 만들어서 상속.

--- app/src/main/java/org/techtown/retrofitlibrary_20211122/BaseActivity.kt
+++ app/src/main/java/org/techtown/retrofitlibrary_20211122/BaseActivity.kt
@@ -1,18 +1,29 @@
 package org.techtown.retrofitlibrary_20211122
 
 import android.content.Context
+import android.content.ServiceConnection
 import android.os.Bundle
 import android.os.PersistableBundle
 import androidx.appcompat.app.AppCompatActivity
+import org.techtown.retrofitlibrary_20211122.api.ServerAPI
+import org.techtown.retrofitlibrary_20211122.api.ServerAPIService
 
 abstract class BaseActivity :AppCompatActivity() {
 
     lateinit var mContext:Context
 
+    lateinit var apiService : ServerAPIService
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         mContext = this
+
+        val retrofit = ServerAPI.getRetrofit()
+        apiService = retrofit.create(ServerAPIService::class.java)
+
+
+
     }
 
 
--- app/src/main/java/org/techtown/retrofitlibrary_20211122/MainActivity.kt
+++ app/src/main/java/org/techtown/retrofitlibrary_20211122/MainActivity.kt
@@ -5,12 +5,42 @@ import android.os.Bundle
 import androidx.databinding.DataBindingUtil
 import org.techtown.retrofitlibrary_20211122.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : BaseActivity() {
 
-    lateinit var binding : ActivityMainBinding
+    lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        setupEvents()
+        setValues()
+
+
     }
+
+    override fun setupEvents() {
+
+
+        binding.btnLogin.setOnClickListener {
+            val inputEmail = binding.edtEmail.text.toString()
+            val inputPw = binding.edtPassword.text.toString()
+
+            apiService.postRequestLogin(inputEmail, inputPw)
+
+
+        }
+    }
+
+    override fun setValues() {
+
+    }
+
+
+}
+
+
+
+
 }
