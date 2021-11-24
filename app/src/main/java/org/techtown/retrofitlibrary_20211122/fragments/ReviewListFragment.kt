1b6e83bc17713a400c1727b4d1b9c4b85fb8c88b
KIMMKyungsik <93589990+KIMMKyungsik@users.noreply.github.com>
2021-11-25 03:55:28 +0900

    프래그먼트 바인딩 완료

--- app/src/main/java/org/techtown/retrofitlibrary_20211122/fragments/ReviewListFragment.kt
+++ app/src/main/java/org/techtown/retrofitlibrary_20211122/fragments/ReviewListFragment.kt
@@ -4,17 +4,22 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import org.techtown.retrofitlibrary_20211122.R
+import org.techtown.retrofitlibrary_20211122.databinding.FragmentReviewListBinding
 
 class ReviewListFragment : Fragment() {
+
+    lateinit var binding : FragmentReviewListBinding
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_review_list,container,false) //암기
+        binding =  DataBindingUtil.inflate( inflater,R.layout.fragment_review_list,container,false)
+    return binding.root // 위에 두줄 암기
 
     }
 
