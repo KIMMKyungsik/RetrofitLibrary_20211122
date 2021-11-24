e7a4c5890e5cbaa0da346cf6cbfcfa6d65fe93a2
KIMMKyungsik <93589990+KIMMKyungsik@users.noreply.github.com>
2021-11-25 06:04:34 +0900

    BaseFragment 클래스 추가

---
+++ app/src/main/java/org/techtown/retrofitlibrary_20211122/fragments/BaseFragment.kt
@@ -0,0 +1,21 @@
+package org.techtown.retrofitlibrary_20211122.fragments
+
+import android.content.Context
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+
+abstract class BaseFragment : Fragment() {
+
+    lateinit var  mContext : Context
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+        mContext = requireContext()
+    }
+
+    abstract fun setupEvents()
+    abstract fun setValues()
+
+
+}
--- app/src/main/java/org/techtown/retrofitlibrary_20211122/fragments/MyProfileFragment.kt
+++ app/src/main/java/org/techtown/retrofitlibrary_20211122/fragments/MyProfileFragment.kt
@@ -1,6 +1,7 @@
 package org.techtown.retrofitlibrary_20211122.fragments
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -8,6 +9,7 @@ import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import org.techtown.retrofitlibrary_20211122.R
 import org.techtown.retrofitlibrary_20211122.databinding.FragmentMyProfileBinding
+import org.techtown.retrofitlibrary_20211122.utils.ContextUtil
 
 class MyProfileFragment :Fragment(){
     lateinit var binding: FragmentMyProfileBinding
@@ -26,7 +28,7 @@ class MyProfileFragment :Fragment(){
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
 
-
+    Log.d("토큰값 확인", ContextUtil.getToken(requireContext()))
 
     }
 
