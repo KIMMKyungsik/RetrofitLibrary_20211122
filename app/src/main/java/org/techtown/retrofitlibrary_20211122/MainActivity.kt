bb46d47664cff59f79d12ef4976e8edfa127950a
KIMMKyungsik <93589990+KIMMKyungsik@users.noreply.github.com>
2021-11-25 05:41:35 +0900

    탭 레이아웃 설정

--- app/src/main/java/org/techtown/retrofitlibrary_20211122/MainActivity.kt
+++ app/src/main/java/org/techtown/retrofitlibrary_20211122/MainActivity.kt
@@ -3,10 +3,12 @@ package org.techtown.retrofitlibrary_20211122
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
+import org.techtown.retrofitlibrary_20211122.adapters.MainViewPagerAdapter
 import org.techtown.retrofitlibrary_20211122.databinding.ActivityMainBinding
 
 class MainActivity : BaseActivity() {
     lateinit var binding : ActivityMainBinding
+    lateinit var mvpa : MainViewPagerAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,6 +22,12 @@ class MainActivity : BaseActivity() {
     }
 
     override fun setValues() {
+//          뷰페이저 메인 엑티비티 자리에 연결
+        mvpa = MainViewPagerAdapter( supportFragmentManager)
+        binding.mainViewPager.adapter = mvpa
+
+        binding.mainTabLayout.setupWithViewPager(binding.mainViewPager)// 탭레이아웃 메인 엑티비티 자리에 연결
+
 
     }
 }
--- app/src/main/java/org/techtown/retrofitlibrary_20211122/adapters/MainViewPagerAdapter.kt
+++ app/src/main/java/org/techtown/retrofitlibrary_20211122/adapters/MainViewPagerAdapter.kt
@@ -12,13 +12,24 @@ class MainViewPagerAdapter(fm: FragmentManager) : FragmentPagerAdapter(fm) {
 
     override fun getCount() = 3
 
+    override fun getPageTitle(position: Int): CharSequence? {
+        return when (position) {
+
+            0 -> "리뷰목록"
+            1 -> "상품목록"
+            else -> "내 프로필"
+
+
+        }
+    }
+
+
     override fun getItem(position: Int): Fragment {
-        return when(position) {
+        return when (position) {
 
             0 -> ReviewListFragment()
             1 -> ProductLIstFragment()
             else -> MyProfileFragment()
-
 
 
         }
